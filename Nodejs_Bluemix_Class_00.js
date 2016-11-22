e453558b3369edbdecbac9780c5142dafa02a716
Alexandre Zanetti <aleza@br.ibm.com>
2016-11-22 15:47:13 -0200

    Terceira

--- Nodejs_Bluemix_Class_00.js
+++ Nodejs_Bluemix_Class_00.js
@@ -1,18 +1,3 @@
-/*
-var http = require('http');
-http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World Zzz\n');
-    
-    
-    app.listen(process.env.PORT || 3000);   
-    
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://localhost:3000/ ou http://127.0.0.1:3000/');
-*/
-
-/*eslint-env node*/
-
 //------------------------------------------------------------------------------
 //node.js starter application for Bluemix
 //------------------------------------------------------------------------------
